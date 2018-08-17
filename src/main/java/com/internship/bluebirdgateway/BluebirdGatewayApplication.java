e6dca052654ae8065017e91c1813bb74c04ea11d
Dragos Sabau <dsabu@xpo.com>
2018-08-17 10:49:29 +0300

    Initial commit

---
+++ src/main/java/com/internship/bluebirdgateway/BluebirdGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.internship.bluebirdgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class BluebirdGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BluebirdGatewayApplication.class, args);
+    }
+}
---
+++ src/test/java/com/internship/bluebirdgateway/BluebirdGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.internship.bluebirdgateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BluebirdGatewayApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
